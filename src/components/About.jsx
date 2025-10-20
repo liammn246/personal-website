a9e063c7cd707c1825bad52256a415b1354a2fd2
Liam Nagel <>
2025-10-19 21:24:18 -0700

    progress

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,16 @@
 function About() {
   return (
-    <section className="flex flex-col-reverse md:flex-row items-center justify-center gap-10 mb-20 text-center md:text-left">
-      {/* Text section */}
+    <section className="flex flex-col-reverse md:flex-row items-center justify-center gap-10 mb-30 text-center md:text-left">
       <div className="text-white mx-auto max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl">
         <h1 className="text-5xl font-semibold mb-4">Hi, I'm Liam</h1>
         <p className="text-2xl">
-          I am a software developer blah blah blah blah blah blah
+          I’m a software developer who loves turning ideas into things people
+          can actually use and enjoy. Whether it’s a web app, a game, or a cool side
+          projects. I am most interested in back end web development, as well as systems 
+          architecture.
         </p>
       </div>
-
-      {/* Image section */}
-      <img
-        src="/images/pfp.jpeg"
-        className="rounded-full w-[180px] h-[180px] sm:w-[200px] sm:h-[200px] md:w-[250px] md:h-[250px] object-cover"
-        alt="Profile"
-      />
+      <img src="/images/pfp.jpeg" className="rounded-full w-[180px] h-[180px] sm:w-[200px] sm:h-[200px] md:w-[250px] md:h-[250px] object-cover" alt="Profile"/>
     </section>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,53 @@
 function Projects() {
   return (
-    <section className="flex flex-col items-center text-center mb-20">
-      <div className="max-w-3xl w-full px-5">
-        <h1 className="text-white text-5xl font-semibold mb-10">Projects</h1>
-        <div className="flex flex-wrap justify-center gap-10">
-            <div className="bg-gray-900 p-5 w-64 flex items-center justify-center rounded-xl shadow-lg">
-                <img src="/images/guy.png" className="w-full h-40 object-cover rounded-lg" alt="Project 1"/>
+    <section className="flex flex-col items-center mb-20 text-left">
+      <div className="max-w-6xl w-full px-6">
+        <h1 className="text-white text-5xl font-semibold mb-10 text-center"> Projects </h1>
+        {/* Project Cards */}
+        <div className="grid md:grid-cols-2 gap-8">
+          <a href="https://github.com/liammn246/CharacterSwipe" target="_blank">
+          {/* Project 1 */}
+          <div className="bg-[#1c1c1f] rounded-xl shadow-lg overflow-hidden border border-[#2e2e33] hover:scale-[1.01] transition-transform duration-300">
+            <img src="/images/aoe2_vill.png" alt="CharacterSwipe" className="w-full h-56 object-cover" />
+            <div className="p-6">
+              <h2 className="text-white text-xl font-bold mb-1">CharacterSwipe</h2>
+              <p className="text-gray-400 text-sm mb-3">Full Stack</p>
+              <p className="text-gray-300 text-sm leading-relaxed mb-4">
+                Developed CharacterSwipe, a mobile game inspired by 2048, with dynamic animations and engaging backgrounds. 
+                Worked with a small team of students through the game development cycle, including design, coding, testing, and release.
+                Gained hands-on experience in mobile game production and user engagement.
+              </p>
+              {/* Tags */}
+              <div className="flex flex-wrap gap-2 mt-2">
+                <span className="text-xs text-gray-300 bg-[#2e2e33] px-2 py-1 rounded-md">Swift</span>
+                <span className="text-xs text-gray-300 bg-[#2e2e33] px-2 py-1 rounded-md">GameplayKit</span>
+                <span className="text-xs text-gray-300 bg-[#2e2e33] px-2 py-1 rounded-md">SwiftUI</span>
+              </div>
             </div>
-            <div className="bg-gray-900 p-5 w-64 flex items-center justify-center rounded-xl shadow-lg">
-                <img src="/images/guy.png" className="w-full h-40 object-cover rounded-lg" alt="Project 2"/>
+          </div>
+          </a>
+          <a href="https://github.com/liammn246/Fullstack-Blog-App" target="_blank">
+          {/* Project 2 */}
+          <div className="bg-[#1c1c1f] rounded-xl shadow-lg overflow-hidden border border-[#2e2e33] hover:scale-[1.01] transition-transform duration-300">
+            <img src="/images/blog_home.png" alt="BlogApp" className="w-full h-56 object-cover" />
+            <div className="p-6">
+              <h2 className="text-white text-xl font-bold mb-1">Blog App</h2>
+              <p className="text-gray-400 text-sm mb-3">Full Stack</p>
+              <p className="text-gray-300 text-sm leading-relaxed mb-4">
+                A full-stack blog website using React, FastAPI, SQLAlchemy. Implemented features such as user authentication, CRUD posts, 
+                managing a database, and responsive design. Gained hands-on experience in 
+                web development, API integration, and deployment.
+              </p>
+              {/* Tags */}
+              <div className="flex flex-wrap gap-2 mt-2">
+                <span className="text-xs text-gray-300 bg-[#2e2e33] px-2 py-1 rounded-md">Django</span>
+                <span className="text-xs text-gray-300 bg-[#2e2e33] px-2 py-1 rounded-md">React</span>
+                <span className="text-xs text-gray-300 bg-[#2e2e33] px-2 py-1 rounded-md">SQLite</span>
+              </div>
             </div>
+          </div>
+          </a>
+
         </div>
       </div>
     </section>
